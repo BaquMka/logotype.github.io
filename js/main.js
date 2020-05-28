e8e78cd739666ed4c3d4b4744f5ea97c8f7ab983
BaquMka <53485064+BaquMka@users.noreply.github.com>
2020-05-28 22:23:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $('.slider__inner, .news__slider__inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    });
+    $('select').styler();
+
+    $('.header__btn__menu').on('click', function(){
+        $('.menu ul').slideToggle();
+    });
+
+
+});
